9f10a412d9e4d1377dd16214c668a57e6f41b0b2
Viraj Jadav <12402130601074@adit.ac.in>
2025-09-12 15:12:09 +0530

    commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
   return (
     <>
     <header className="sticky top-0 z-50 bg-white shadow-md border-b border-gray-200">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 gap-4">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-3 flex-shrink-0 mr-8">
